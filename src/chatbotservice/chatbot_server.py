8ecbd2995fe4296d5f86ae1335a37eaa21bcd193
Oliver <o.cheung@protonmail.com>
2021-02-22 11:04:54 +0100

    Changes monitoring, adds requests benchmarking

--- src/chatbotservice/chatbot_server.py
+++ src/chatbotservice/chatbot_server.py
@@ -4,21 +4,7 @@ Author: Ming Yeh Oliver Cheung
 
 Copyright (c) 2021 Wise CSE Group 1
 '''
-#!/usr/bin/python
-#
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
 
 import os, logging
 import random
@@ -38,11 +24,12 @@ from grpc_health.v1 import health_pb2_grpc
 
 class ChatbotService(demo_pb2_grpc.ChatbotServiceServicer):
     def getChatbotMessage(self, request, context):
+        monitor.start_request()
         response = demo_pb2.chatbotResponse(message = "", product_ids = [""])
         response.message = chatbot.handle_message(request.message, request.user_id)
+        monitor.stop_request()
         return response
 
-#from recomendation_server.py        
     def Check(self, request, context):
         return health_pb2.HealthCheckResponse(
             status=health_pb2.HealthCheckResponse.SERVING)
@@ -53,7 +40,10 @@ class ChatbotService(demo_pb2_grpc.ChatbotServiceServicer):
 
 
 if __name__ == "__main__":
+    """ Starts a chatbot server over grpc at port 9090
+    """
     global chatbot
+    global monitor
 
     logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s',
                         filename='logs.txt',
@@ -62,28 +52,18 @@ if __name__ == "__main__":
     monitor = Monitor(4)
 
 
-    #server setup
     chatbot = MetaEngine()
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    # port = "50051"
     port = "9090"
-    #port = os.environ.get('Port', "9090")
     
-    
-    # add class to gRPC server
     service = ChatbotService()
     demo_pb2_grpc.add_ChatbotServiceServicer_to_server(service, server)
     health_pb2_grpc.add_HealthServicer_to_server(service, server)
 
-    #start server
     logging.info("listening on port: " +port)
     server.add_insecure_port('[::]:'+port)
     server.start()
-    #server.wait_for_termination()
 
-    
-
-    # keep alive
     try:
          while True:
             time.sleep(10000)
--- src/chatbotservice/monitor.py
+++ src/chatbotservice/monitor.py
@@ -16,6 +16,12 @@ import tracemalloc
 import prometheus_client
 
 class Monitor(Thread):
+    """ Monitors a process, by cpu usage, ram usage
+        and can also measure request time. Results are opened on a prometheus
+        client where it can be scrapped to a server on port 9999
+    Args:
+        Thread ([type]): [description]
+    """
     def __init__(self, delay):
         super(Monitor, self).__init__()
         self.system_usage = prometheus_client.Gauge('system_usage',
@@ -23,21 +29,27 @@ class Monitor(Thread):
                             ['resource_type'])
         prometheus_client.start_http_server(9999)
         self.stopped = False
+        self.started_request = False
+        self.request_time = 0.0
+        self.all_request_time = 0.0
+        self.requests = 0
+        self.stopped = False
         self.delay = delay # Time between calls to GPUtil
         tracemalloc.start()
         self.start_time = None
         self.start()
 
     def run(self):
-
         while not self.stopped:
             current_ram, ath_ram = tracemalloc.get_traced_memory()
-            current_ram = current_ram / 10**6
-            values = psutil.virtual_memory()
+            current_ram = current_ram 
             self.system_usage.labels('CPU').set(psutil.cpu_percent())
-            self.system_usage.labels('Memory').set(values.total >> 20)
-            self.system_usage.labels("Process_Memory").set(current_ram)
-            self.system_usage.labels("Peak_Memory").set(ath_ram)
+            self.system_usage.labels("Process_Memory").set(current_ram/ 10**6)
+            self.system_usage.labels("Peak_Memory").set(ath_ram/ 10**6)
+            if self.started_request:
+                request_average = self.all_request_time/self.requests
+                self.system_usage.labels("Average Request Time").set(request_average)
+                self.system_usage.labels("Latest Request Time").set(self.request_time)
             time.sleep(self.delay)
 
     def stop(self):
@@ -46,6 +58,20 @@ class Monitor(Thread):
         tracemalloc.stop()
         self.stopped = True
 
+    def start_request(self):
+        self.start_time = time.perf_counter()
+
+    def stop_request(self):
+        self.stop_time = time.perf_counter() - self.start_time
+        self.set_request_time(self.stop_time)
+
+    def set_request_time(self, time):
+        self.request_time = time
+        self.all_request_time += time
+        self.requests += 1
+        self.started_request = True
 
 if __name__ == "__main__":
     mon = Monitor(1)
+    mon.start_request()
+    mon.stop_request()
